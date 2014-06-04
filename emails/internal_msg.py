ae8ca6c75770d5cad0ac2aa2de3c805313fd54d9
Michael Flaxman <mflaxman@gmail.com>
2014-06-04 12:01:53 -0300

    add dev prefix to internal messages

--- emails/internal_msg.py
+++ emails/internal_msg.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 
-from bitcash.settings import ADMINS, SERVER_EMAIL
+from bitcash.settings import ADMINS, SERVER_EMAIL, EMAIL_DEV_PREFIX
 
 
 def get_admin_emails():
@@ -8,8 +8,14 @@ def get_admin_emails():
 
 
 def send_admin_email(subject, message, recipient_list=None):
+    " Send an admin email "
+
     if not recipient_list:
         recipient_list = get_admin_emails()
+
+    if EMAIL_DEV_PREFIX:
+        subject += ' [DEV]'
+
     send_mail(
             subject=subject,
             message=message,

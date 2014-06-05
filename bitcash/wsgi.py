d4b2c30c9abedfec912b4c22c52ca5ce5eaca3c0
Michael Flaxman <mflaxman@gmail.com>
2014-06-05 13:19:43 -0300

    reorder import

--- bitcash/wsgi.py
+++ bitcash/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-from dj_static import Cling
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bitcash.settings")
+
+from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())

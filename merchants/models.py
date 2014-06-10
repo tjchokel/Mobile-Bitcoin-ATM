db018467beb2e96c97d999125cffe9a160f2884c
Michael Flaxman <mflaxman@gmail.com>
2014-06-10 18:09:26 -0300

    improve country (and phone) display)

--- merchants/models.py
+++ merchants/models.py
@@ -4,7 +4,7 @@ from django.utils.timezone import now
 from phonenumber_field.modelfields import PhoneNumberField
 from bitcoins.models import DestinationAddress
 
-from countries import BFHCurrenciesList
+from countries import BFHCurrenciesList, ALL_COUNTRIES
 
 
 class Merchant(models.Model):
@@ -14,7 +14,7 @@ class Merchant(models.Model):
     address_2 = models.CharField(max_length=256, blank=True, null=True, db_index=True)
     city = models.CharField(max_length=256, blank=False, null=False, db_index=True)
     state = models.CharField(max_length=30, null=True, blank=True, db_index=True)
-    country = models.CharField(max_length=256, blank=True, null=True, db_index=True)
+    country = models.CharField(max_length=256, blank=True, null=True, db_index=True, choices=ALL_COUNTRIES)
     zip_code = models.CharField(max_length=256, blank=True, null=True, db_index=True)
     phone_num = PhoneNumberField(blank=True, null=True, db_index=True)
     hours = models.CharField(max_length=256, blank=True, null=True, db_index=True)

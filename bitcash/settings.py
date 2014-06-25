a5a90731d3aa25b893b0cc7a06f79817e9c52748
Michael Flaxman <mflaxman@gmail.com>
2014-06-24 18:08:48 -0700

    no disabling loggers

--- bitcash/settings.py
+++ bitcash/settings.py
@@ -196,7 +196,7 @@ CAPITAL_CONTROL_COUNTRIES = ['ARS', 'VEF']
 # http://scanova.io/blog/engineering/2014/05/21/error-logging-in-javascript-and-python-using-sentry/
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
     'root': {
         'level': 'WARNING',
         'handlers': ['sentry'],

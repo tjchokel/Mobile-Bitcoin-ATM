c3da2b2745b34cc91cd728792df90c5c691981a8
Michael Flaxman <mflaxman@gmail.com>
2014-06-24 18:18:33 -0700

    add docs link

--- bitcash/wsgi.py
+++ bitcash/wsgi.py
@@ -1,5 +1,7 @@
 # https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+# http://raven.readthedocs.org/en/latest/config/django.html#wsgi-middleware
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bitcash.settings")

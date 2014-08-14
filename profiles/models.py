f958f8107f5e9d3312bd27511dfab5eb946e3063
Michael Flaxman <mflaxman@gmail.com>
2014-08-13 18:44:22 -0700

    basic ShortURLs for account creation (doesn't support editing). not tested

--- profiles/models.py
+++ profiles/models.py
@@ -6,8 +6,8 @@ from bitcash.settings import BASE_URL
 
 class ShortURL(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    # only allow lowercase to be saved into the database to avoid collisions:
     uri_display = models.CharField(max_length=256, blank=False, null=False, db_index=True)
+    # used to enforce no collissions at the database level:
     uri_lowercase = models.CharField(max_length=256, blank=False, null=False, unique=True, db_index=True)
     merchant = models.ForeignKey('merchants.Merchant', blank=False, null=False)
     deleted_at = models.DateTimeField(db_index=True, blank=True, null=True)
@@ -19,7 +19,6 @@ class ShortURL(models.Model):
         """
         Enforce uri_display and uri_lowercase logic
         """
-        # This only happens if the objects isn't in the database yet.
         self.uri_lowercase = self.uri_display.lower()
         super(ShortURL, self).save(*args, **kwargs)
 

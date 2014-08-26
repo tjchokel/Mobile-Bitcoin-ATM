d9f06e2b90af6f15e11cc6cedfca28b92f2ace26
Michael Flaxman <mflaxman@gmail.com>
2014-08-26 14:02:11 -0700

    default mailgun domain

--- bitcash/settings.py
+++ bitcash/settings.py
@@ -212,7 +212,7 @@ assert POSTMARK_API_KEY, 'Must have a Postmark API Key'
 
 # For marketing messages:
 MAILGUN_API_KEY = os.getenv('MAILGUN_API_KEY')
-MAILGUN_DOMAIN = os.getenv('MAILGUN_DOMAIN')
+MAILGUN_DOMAIN = os.getenv('MAILGUN_DOMAIN', 'coinsafe.com')
 assert MAILGUN_API_KEY, 'Must have a Mailgun API Key'
 assert MAILGUN_DOMAIN, 'Must have a Mailgun Domain'
 

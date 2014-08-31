859bbceb713a4e58fb417319d3a553b1ace7bb71
Tom Chokel <tjchokel@gmail.com>
2014-08-31 10:55:56 -0700

    some more work on the blog

--- blog/views.py
+++ blog/views.py
@@ -16,15 +16,19 @@ def blog_index(request):
 
 @render_to('view_post.html')
 def view_post(request, slug):
+    categories = Category.objects.all()
     return {
-        'post': get_object_or_404(BlogPost, slug=slug)
+        'post': get_object_or_404(BlogPost, slug=slug),
+        'categories': categories,
     }
 
 
 @render_to('view_category.html')
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
+    categories = Category.objects.all()
     return {
         'category': category,
-        'posts': BlogPost.objects.filter(category=category)[:5]
+        'posts': BlogPost.objects.filter(category=category)[:5],
+        'categories': categories,
     }

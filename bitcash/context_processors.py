02808057539b1ba08c76176f0f7b0fe3bc4f3ab0
Michael Flaxman <mflaxman@gmail.com>
2014-09-03 10:18:59 -0700

    add support for no balance

--- bitcash/context_processors.py
+++ bitcash/context_processors.py
@@ -3,6 +3,7 @@ def credential_status(request):
 
     has_cred = False
     cred_appears_usable = None
+    appears_to_have_btc = False
 
     user = request.user
     if user.is_authenticated():
@@ -12,5 +13,13 @@ def credential_status(request):
             if credential:
                 has_cred = True
                 cred_appears_usable = credential.appears_usable()
+                latest_balance = credential.get_latest_balance()
+                if latest_balance:
+                    if latest_balance.satoshis > 0:
+                        appears_to_have_btc = True
 
-    return {'has_cred': has_cred, 'cred_appears_usable': cred_appears_usable}
+    return {
+            'has_cred': has_cred,
+            'cred_appears_usable': cred_appears_usable,
+            'appears_to_have_btc': appears_to_have_btc,
+            }

369cbf10c7e1da70d0669c2e96362a85b3482d77
Tom Chokel <tjchokel@gmail.com>
2014-09-04 11:17:04 -0700

    removing view from blog url

--- bitcash/urls.py
+++ bitcash/urls.py
@@ -61,8 +61,8 @@ urlpatterns = patterns('',
     url(r'^press/', TemplateView.as_view(template_name='fixed_pages/press.html'), name='press'),
 
     # blog
-    url(r'^blog/view/(?P<slug>[^\.]+)', 'blog.views.view_post', name='view_blog_post'),
     url(r'^blog/category/(?P<slug>[^\.]+)', 'blog.views.view_category', name='view_blog_category'),
+    url(r'^blog/(?P<slug>[^\.]+)', 'blog.views.view_post', name='view_blog_post'),
     url(r'^blog/', 'blog.views.blog_index', name='blog_index'),
 
     url(r'^admin/', include(admin.site.urls)),

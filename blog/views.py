f1242947ac12eae1fd4071e1ced54b0a44c78d6e
Michael Flaxman <mflaxman@gmail.com>
2014-09-05 09:37:29 -0700

    move blog templates to own directory and hide recently added when there are few blogs posts

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from blog.models import BlogPost, Category
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import get_object_or_404
 
 from annoying.decorators import render_to
 
 
-@render_to('blog_index.html')
+@render_to('blog/blog_index.html')
 def blog_index(request):
     categories = Category.objects.all()
     return {
@@ -14,7 +13,7 @@ def blog_index(request):
     }
 
 
-@render_to('view_post.html')
+@render_to('blog/view_post.html')
 def view_post(request, slug):
     categories = Category.objects.all()
     return {
@@ -24,7 +23,7 @@ def view_post(request, slug):
     }
 
 
-@render_to('view_category.html')
+@render_to('blog/view_category.html')
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
     categories = Category.objects.all()

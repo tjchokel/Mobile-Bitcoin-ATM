87163a93e94472b654ab85cecc94540ef62fcab4
Michael Flaxman <mflaxman@gmail.com>
2014-09-18 13:23:31 -0700

    fix config for gunicorn

--- bitcash/settings.py
+++ bitcash/settings.py
@@ -130,13 +130,15 @@ if SITE_DOMAIN in (PRODUCTION_DOMAIN, STAGING_DOMAIN):
 else:
     BASE_URL = 'http://%s' % SITE_DOMAIN
 
-if SITE_DOMAIN == PRODUCTION_DOMAIN:
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+IS_PRODUCTION = (SITE_DOMAIN == PRODUCTION_DOMAIN)
+
+if IS_PRODUCTION:
     EMAIL_DEV_PREFIX = False
-    DEBUG_TOOLBAR_PATCH_SETTINGS = False
 else:
     EMAIL_DEV_PREFIX = True
     # Enable debug toolbar on local and staging
-    DEBUG_TOOLBAR_PATCH_SETTINGS = True
     MIDDLEWARE_CLASSES = ('debug_toolbar.middleware.DebugToolbarMiddleware',) + MIDDLEWARE_CLASSES
     INSTALLED_APPS += ('debug_toolbar', )
 
